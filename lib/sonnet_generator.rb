7617290dc999ec8f39ff54b817118a66a18645a0
Lydia B <lydiabaik@gmail.com>
2020-02-07 03:24:51 +0000

    first commit

---
+++ lib/sonnet_generator.rb
@@ -0,0 +1,6 @@
+require "sonnet_generator/version"
+
+module SonnetGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sonnet_generator/version.rb
@@ -0,0 +1,3 @@
+module SonnetGenerator
+  VERSION = "0.1.0"
+end
