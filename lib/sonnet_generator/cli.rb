9c7b626c116fd0affccc67226440df74a6e760ce
Lydia B <lydiabaik@gmail.com>
2020-02-08 23:41:41 +0000

    created sub menu

--- lib/sonnet_generator/cli.rb
+++ lib/sonnet_generator/cli.rb
@@ -15,13 +15,16 @@ class SonnetGenerator::CLI
   def menu
     input = gets.strip.downcase
     if input == "yes"
-      poet_list
-      menu
-    elsif input == "no" || input == "adieu" 
+      generate_poem(@@name)
+    elsif input == "no" || input == "adieu" || input == "exit"
       goodbye
     else
       invalid_entry
     end
+  end
+  
+    def goodbye
+    puts "Adieu, #{@@name}. Till next time."
   end
   
   def invalid_entry
@@ -29,36 +32,34 @@ class SonnetGenerator::CLI
       menu
   end
   
-  def poet_list
+  def generate_poem(name)
     puts "To the fairest, #{@@name}..."
-
-# puts"             _______________________"
-# puts"           =(__    ___      __     _)="
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |                     |"
-# puts"             |__    ___   __    ___|"
-# puts"           =(_______________________)="
-           
-           
-          
     input = gets.strip.downcase
-    poet_selection(input)
+    puts "-------------------------------------------------------"
+    puts "To learn more about your custom sonnet, type 'View'"
+    puts "To generate a new poem, type 'New'"
+    puts "To exit, type 'Adieu'"
+    sub_menu(input)
   end
   
-  def poet_selection(poet)
-    puts "#{poet}"
-    #go over array and find starship
+  
+  def sub_menu(sub_input)
+    if sub_input == "view"
+      learn_more
+    elsif sub_input == "new"
+      generate_poem(@@name)
+    elsif sub_input == "adieu"
+      goodbye
+    else
+      invalid_entry
+    end
   end
   
-  def goodbye
-    puts "Adieu, #{@@name}. Till next time."
+  def learn_more
+    puts "Your custom sonnet is comprised of Shakespeare's Sonnets: List of Sonnets"
+    puts " To read the original Sonnet, type in the sonnet number (i.e. 1, 2, 3)"
   end
+  
+
   
 end
